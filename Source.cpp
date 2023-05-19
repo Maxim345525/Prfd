ce61eb228919b50ba7722da3f721523c681c2142
Maxim345525 <maximmaksimchyk@gmail.com>
2023-05-19 12:04:34 +0300

    add code

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,21 @@
-dtr
+#include<iostream>
+#include <fstream>
+#include <string>
+#include<ostream>
+using namespace std;
+int main() {
+    std::string filename;
+    std::cout << "Input name of file: ";
+    std::cin >> filename;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error." << std::endl;
+        return 1;
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+    file.close();
+    return 0;
+}
